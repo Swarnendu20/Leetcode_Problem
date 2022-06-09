197dcdde84d2505dcc818001119b9db4fd9d8974
Swarnendu Mondal <105766554+Swarnendu20@users.noreply.github.com>
2022-06-09 11:00:35 +0530

    Add files via upload

---
+++ 167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,30 @@
+
+  //   https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+
+
+class Solution {
+public:
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        vec.push_back(root->val);
+        inorder(root->right);
+    }
+    
+    bool findTarget(TreeNode* root, int k) {
+        inorder(root);
+        int l = 0, r = vec.size()-1;
+        while (l < r) {
+            if (vec[l] + vec[r] == k) return true;
+            else {
+                if (vec[l] + vec[r] < k) l++;
+                else r--;
+            }
+        }
+        return false;
+    }
+    
+private:
+    vector<int> vec;
+};
